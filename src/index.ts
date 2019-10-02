07c7880ba3c372590a19da17966a46e8e1eb21af
Raphael Kieling <rapha_kieling@hotmail.com>
2019-10-01 21:42:47 -0300

    Finalizado preferencias

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import axios from 'axios';
 import * as mongoose from 'mongoose';
 import Product from './models/Product';
 import config from './config';
+import { CronJob } from 'cron';
 
 const app = express();
 app.use(helmet());
@@ -20,6 +21,18 @@ function request() {
     })
 }
 
+app.get('/item', async (req, res) => {
+    const { user_email } = req.headers;
+    let products = await Product.find({ 'created_by.email': user_email })
+    res.send(products);
+});
+
+app.get('/item/:id', async (req, res) => {
+    const { id } = req.params;
+    let product = await Product.findById(id);
+    res.send(product);
+});
+
 app.get('/search', async (req, res) => {
     const query = req.query;
     const { data } = await request().get(`sites/MLB/search?q=${query.q}`);
@@ -27,18 +40,30 @@ app.get('/search', async (req, res) => {
 });
 
 app.post('/save', async (req, res) => {
+    const { user_email } = req.headers;
     const { data } = await request().get(`items?ids=${req.query.ids}`);
     const products = data
         .filter(item => item.code === 200)
-        .map(item => item.body)
+        .map(item => item.body);
 
     for (let item of products) {
         const ProductInstance = new Product({
             ...item,
-            old_price: item.base_price,
+            base_price: item.base_price,
+            last_price: item.price,
+            price: item.price,
+            created_by: {
+                email: user_email
+            },
             currency: item.currency_id,
             quantity: item.available_quantity,
-            link: item.permalink
+            link: item.permalink,
+            price_history: [
+                {
+                    price: item.price,
+                    date: Date.now()
+                }
+            ]
         });
 
         await ProductInstance.save();
@@ -51,9 +76,37 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect(config.mongodburi, { useNewUrlParser: true });
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-mongoose.connection.once('open', function () {
+mongoose.connection.once('open', onOpenConnection);
+
+function onOpenConnection() {
     app.listen(PORT, () => {
         console.log(`Running on port [${PORT}]`)
     })
-});
+
+    new CronJob('1 * * * * *', function () {
+        console.log("[LOG]: cronjob");
+
+        Product.find(async (err, products: any[]) => {
+            for (let product of products) {
+                const { data } = await request().get(`items?id=${product.id}`);
+
+                if (product.last_price == data.price) {
+                    console.log('[IGNORE]: Same price')
+                    return;
+                }
+
+                product.last_price = product.price;
+                product.price = data.price;
+                product.quantity = data.available_quantity;
+                product.price_history.push({
+                    price: product.price,
+                    date: Date.now()
+                });
+
+                await product.save();
+            }
+        })
+    }, null, true, 'America/Sao_Paulo');
+
+}
 
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,14 +2,23 @@ import * as mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
     id: String,
+    created_by: {
+        email: String
+    },
     title: String,
     price: Number,
-    old_price: Number,
+    last_price: Number,
     currency: String,
     quantity: Number,
     condition: String,
     link: String,
-    thumbnail: String
+    thumbnail: String,
+    price_history: [
+        {
+            price: Number,
+            date: Date
+        }
+    ]
 })
 
 const Product = mongoose.model('Product', productSchema);
