6f8558af3cd49196af8461a0d0583219578d8817
Raphael Kieling <rapha_kieling@hotmail.com>
2019-10-01 22:17:50 -0300

    fix config

--- src/config.dist.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    mongodburi: "mongodb://<user>:<password>@ds239692.mlab.com:39692/mercado-libre-watch"
-}
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export default {
+    mongodburi: "mongodb://root:1mbitubA@ds239692.mlab.com:39692/mercado-libre-watch"
+}
